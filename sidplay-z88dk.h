7db34e4b5295216647494229ff6d78b32ec7372d
Justin Skists <justin.skists@juzza.co.uk>
2021-07-11 17:57:55 +0100

    start trimming for rc2014

---
+++ sidplay-z88dk.h
@@ -0,0 +1,10 @@
+//
+// Created by justin on 10/07/2021.
+//
+
+#ifndef SIDPLAY_Z88DK_H
+#define SIDPLAY_Z88DK_H
+
+extern void sidplay_init(void *sid_file) __z88dk_callee;
+
+#endif //SIDPLAY_Z88DK_H
